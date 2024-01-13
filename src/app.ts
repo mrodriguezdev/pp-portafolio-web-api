23e769602bf9c4da727453dfcc501c35c9eaa099
mrodriguezdev <marioguzman_99@hotmail.com>
2024-01-12 22:18:30 -0600

    Agregando importación dinámica de rutas
    Creando archivo index.ts y email.ts que servirá para el envió de correos

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import { router } from "./routes/index";
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(cors());
+app.use(router)
 
 app.listen(PORT, () => console.log(`Server listening 🚀🚀`));
 
